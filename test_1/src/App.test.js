35c5d2ddb79c954b7db90d4baffcf031e7df8789
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-07 20:29:11 +0530

    getByText apply in the testing

--- test_1/src/App.test.js
+++ test_1/src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 it("should render", () => {
   render(<App />);
-  const p_elem = screen.getByText("Create React App");
+  const p_elem = screen.getByText(/Create react App/i);
   expect(p_elem).toBeInTheDocument();
 });
