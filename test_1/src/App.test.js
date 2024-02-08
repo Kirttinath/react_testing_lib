08d7ccb1ee24c9ef88b5bb94a7cc9fbf6993cc34
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-08 14:55:17 +0530

    before and after hook implementation

--- test_1/src/App.test.js
+++ test_1/src/App.test.js
@@ -1,10 +1,36 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
-describe("Fire Event Check", () => {
-  test("button update check", () => {
-    render(<App />);
-    const buttn = screen.getByRole("button");
-    fireEvent.click(buttn);
-    expect(screen.getByText(/update data/i)).toBeInTheDocument();
-  });
+import dataclean from "./dbclean";
+
+// beforeAll(() => {
+//   console.log("beforeAll Hook Called");
+// });
+
+beforeEach(() => {
+  // console.log("beforeEach Hook Called");
+  dataclean();
 });
+
+test("button update check test1", () => {
+  console.log("1");
+  render(<App />);
+  const buttn = screen.getByRole("button");
+  fireEvent.click(buttn);
+  expect(screen.getByText(/update data/i)).toBeInTheDocument();
+});
+
+test("button update check test2", () => {
+  console.log("2");
+  render(<App />);
+  const buttn = screen.getByRole("button");
+  fireEvent.click(buttn);
+  expect(screen.getByText(/update data/i)).toBeInTheDocument();
+});
+
+// afterAll(() => {
+//   console.log("afterAll hook test case");
+// });
+
+// afterEach(() => {
+//   console.log("afterAll hook test case");
+// });
