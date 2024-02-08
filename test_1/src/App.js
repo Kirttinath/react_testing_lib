77a40d620f75f96ed6cf926144d91f7b15198ba3
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-08 11:44:26 +0530

    test cases testing

--- test_1/src/App.js
+++ test_1/src/App.js
@@ -1,10 +1,14 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable react/jsx-no-comment-textnodes */
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <p>Create React App</p>
+      <p>create react app</p>
+      <input type="text" placeholder="name" value={"kirtti"} />
+      <img title="React Logo" src={logo} />
     </div>
   );
 }
--- test_1/src/App.test.js
+++ test_1/src/App.test.js
@@ -4,5 +4,16 @@ import App from "./App";
 it("should render", () => {
   render(<App />);
   const p_elem = screen.getByText(/Create react App/i);
+  const title = screen.getByTitle(/React logo/i);
   expect(p_elem).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
+});
+
+test("should be a input", () => {
+  render(<App />);
+  const input = screen.getByRole("textbox");
+  const inputplaceholder = screen.getByPlaceholderText("name");
+  expect(input).toBeInTheDocument();
+  expect(inputplaceholder).toBeInTheDocument();
+  expect(input).toHaveAttribute("type", "text");
 });
