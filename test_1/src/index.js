016dc9316e9fcac1d7bea59e3c8529b50d8bc233
Kirttinath Ojha <kirttinathojha000@gmail.com>
2024-02-09 09:56:36 +0530

    index.js changes the basic style

--- test_1/src/index.js
+++ test_1/src/index.js
@@ -41,7 +41,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//todo If you want to start measuring performance in your app, pass a function
+//todo to log results (for example: reportWebVitals(console.log))
+//todo or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
